26c798094c96b201ced340d75040b5cacfff5807
slice_of_code <slice_of_code@gmail.com>
2023-02-11 18:44:28 +0400

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [a, setA] = useState(1) // 1. Объявляем хук с переменной и его изменяемым состоянием, задаём начальное значение
+
+    const counterPlus = () => {
+        setA(++a) // 2. Сетаем состояние и указываем новое значение переменной
+    }
+
+    const counterZero = () => {
+        setA(0)
+    }
+
+    return (
+        <div className="App">
+            <h1>{a}</h1>
+            <button onClick={counterPlus}>+ 1</button> {/* 3. Передаём функцию с сетом в событие */}
+            <button onClick={counterZero}>zero</button>
+        </div>
+    );
 }
 
 export default App;
